0dbffdb6df532b73b75bbb7eb7ed0a4f9d48db8e
Angel Paunchev <apaunchev@gmail.com>
2020-01-03 11:48:27 +0200

    remove link

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,7 @@ import Link from "./Link";
 export default () => (
   <header className="Header">
     <div className="Header__left">
-      <Link href="/">
-        <a>
-          <img className="Avatar" src="/images/me.jpg" alt="Photo of Angel" />
-        </a>
-      </Link>
+      <img className="Avatar" src="/images/me.jpg" alt="Photo of Angel" />
     </div>
     <div className="Header__right">
       <h1 className="Title">Angel Paunchev</h1>
