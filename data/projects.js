1c55d235e72095a10a35e50919f2dceda196af40
Angel Paunchev <apaunchev@gmail.com>
2020-04-09 23:10:52 +0300

    update urls

--- data/projects.js
+++ data/projects.js
@@ -11,23 +11,23 @@ export default [
     name: "Spender",
     description:
       "Spender is an app that helps you track how much you spend on subscriptions such as streaming services, mobile carriers, broadband and more. It will break down your spending per week, month or year, and will automatically convert currencies when needed.",
-    url: "https://spender.netlify.app/",
+    url: "https://spender.netlify.app",
     repo: "apaunchev/Spender",
-    img: "Spender.png"
+    img: "Spender.png",
   },
   {
     name: "GitObserve",
     description:
       "GitObserve is a tool that lets you track open pull requests and releases across your watched GitHub repositories. It was born out of necessity to keep up with pending work within a software development team.",
-    url: "https://apaunchev.github.io/GitObserve/",
+    url: "https://apaunchev.github.io/GitObserve",
     repo: "apaunchev/GitObserve",
-    img: "GitObserve.png"
+    img: "GitObserve.png",
   },
   {
     name: "Teamboard",
     description:
       "Teamboard is a dashboard tool that helps you aggregate and visualize metrics about your software development team. It comes with integrations for Jira and GitHub, and can easily be extended to support other services.",
     repo: "apaunchev/Teamboard",
-    img: "Teamboard.png"
+    img: "Teamboard.png",
   },
 ];
