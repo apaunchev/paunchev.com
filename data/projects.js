16c8f44a196edd6dfc7dd8e26b4fcd18e68285b3
Angel Paunchev <apaunchev@gmail.com>
2020-05-03 11:21:47 +0300

    update project descriptions

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,7 @@ export default [
   {
     name: "Budgeter",
     description:
-      "Budgeter is a personal finance tool designed to help you build healthy financial habits by visualizing your income, expenses and savings all in one place.",
+      "Budgeter is a personal finance app that helps you build healthy financial habits by visualizing your income, expenses and savings in one place.",
     url: "https://apaunchev.github.io/Budgeter",
     repo: "apaunchev/Budgeter",
     img: "Budgeter.png",
@@ -10,7 +10,7 @@ export default [
   {
     name: "Spender",
     description:
-      "Spender is an app that helps you track how much you spend on subscriptions such as streaming services, mobile carriers, broadband and more. It will break down your spending per week, month or year, and will automatically convert currencies when needed.",
+      "Spender is a personal finance app that helps you track how much you spend on subscriptions and other fixed expenses.",
     url: "https://spender.netlify.app",
     repo: "apaunchev/Spender",
     img: "Spender.png",
@@ -18,7 +18,7 @@ export default [
   {
     name: "GitObserve",
     description:
-      "GitObserve is a tool that lets you track open pull requests and releases across your watched GitHub repositories. It was born out of necessity to keep up with pending work within a software development team.",
+      "GitObserve is a tool for tracking open pull requests and releases across multiple GitHub repositories.",
     url: "https://apaunchev.github.io/GitObserve",
     repo: "apaunchev/GitObserve",
     img: "GitObserve.png",
@@ -26,7 +26,7 @@ export default [
   {
     name: "Teamboard",
     description:
-      "Teamboard is a dashboard tool that helps you aggregate and visualize metrics about your software development team. It comes with integrations for Jira and GitHub, and can easily be extended to support other services.",
+      "Teamboard is a dashboard app that can be used to aggregate and visualize metrics about a software development organization.",
     repo: "apaunchev/Teamboard",
     img: "Teamboard.png",
   },
