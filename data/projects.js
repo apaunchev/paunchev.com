8ad23576b728a49ae1183640a0f761d051de084d
Angel Paunchev <apaunchev@gmail.com>
2020-05-20 13:14:48 +0300

    add Daily Splash, reorder

--- data/projects.js
+++ data/projects.js
@@ -1,13 +1,5 @@
 export default [
   {
-    name: "Budgeter",
-    description:
-      "Budgeter is a personal finance app that helps you build healthy financial habits by visualizing your income, expenses and savings in one place.",
-    url: "https://apaunchev.github.io/Budgeter",
-    repo: "apaunchev/Budgeter",
-    img: "Budgeter.png",
-  },
-  {
     name: "Spender",
     description:
       "Spender is a personal finance app that helps you track how much you spend on subscriptions and other fixed expenses.",
@@ -16,12 +8,29 @@ export default [
     img: "Spender.png",
   },
   {
+    name: "Daily Splash",
+    description:
+      "Personalize your New Tab page with beautiful photos and customizable widgets.",
+    url:
+      "https://chrome.google.com/webstore/detail/daily-splash/mhllelegiafiipppnijnbombjinhceho",
+    repo: "apaunchev/DailySplash",
+    img: "DailySplash.png",
+  },
+  {
     name: "GitObserve",
     description:
       "GitObserve is a tool for tracking open pull requests and releases across multiple GitHub repositories.",
     url: "https://apaunchev.github.io/GitObserve",
     repo: "apaunchev/GitObserve",
     img: "GitObserve.png",
+  },
+  {
+    name: "Budgeter",
+    description:
+      "Budgeter is a personal finance app that helps you build healthy financial habits by visualizing your income, expenses and savings in one place.",
+    url: "https://apaunchev.github.io/Budgeter",
+    repo: "apaunchev/Budgeter",
+    img: "Budgeter.png",
   },
   {
     name: "Teamboard",
