fb998303929f1db506fbb8d7b6dfb2f6a72fb262
Angel Paunchev <apaunchev@gmail.com>
2020-11-22 15:24:00 +0200

    Update header

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,42 @@
-import SiteLink from "./SiteLink";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Header = () => (
-  <header className="Header">
-    <div>
-      <img className="Avatar" src="/images/me.jpg" alt="Photo of Angel" />
-    </div>
-    <div>
-      <h1 className="Title">Angel Paunchev</h1>
-      <nav className="Navigation">
-        <SiteLink href="/">
-          <a>About</a>
-        </SiteLink>
-        <SiteLink href="/projects">
-          <a>Projects</a>
-        </SiteLink>
-        <SiteLink href="/posts">
-          <a>Blog</a>
-        </SiteLink>
-        <SiteLink href="/books">
-          <a>Books</a>
-        </SiteLink>
-        <SiteLink href="/bookmarks">
-          <a>Bookmarks</a>
-        </SiteLink>
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header className="Header">
+      <div>
+        <img className="Avatar" src="/images/me.jpg" alt="Photo of Angel" />
+      </div>
+      <div>
+        <h1 className="Title">Angel Paunchev</h1>
+        <nav className="Navigation">
+          <Link href="/">
+            <a className={router.pathname === "/" ? "active" : ""}>About</a>
+          </Link>
+          <Link href="/projects">
+            <a className={router.pathname === "/projects" ? "active" : ""}>
+              Projects
+            </a>
+          </Link>
+          <Link href="/posts">
+            <a className={router.pathname === "/posts" ? "active" : ""}>
+              Notes
+            </a>
+          </Link>
+          <Link href="/bookmarks">
+            <a className={router.pathname === "/bookmarks" ? "active" : ""}>
+              Bookmarks
+            </a>
+          </Link>
+          <Link href="/cv">
+            <a>CV</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
