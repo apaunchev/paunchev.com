ff93b59c2f71279d3cfd973b352f1fee9b01ee09
Angel Paunchev <apaunchev@gmail.com>
2021-01-22 22:37:36 +0200

    Update library

--- components/LibraryItem.js
+++ components/LibraryItem.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function LibraryItem({
   type,
   title,
@@ -8,7 +10,7 @@ export default function LibraryItem({
 }) {
   return (
     <a
-      className="group focus:ring-2 focus:ring-purple-600 ring-offset-2 outline-none rounded transition"
+      className="focus:ring-2 focus:ring-purple-600 ring-offset-2 outline-none rounded transition"
       href={url}
       target="_blank"
       rel="noopener"
@@ -19,17 +21,17 @@ export default function LibraryItem({
         </small>
         <div className="flex items-start">
           {image ? (
-            <img
-              className="mr-4"
-              src={image.src}
-              width={80}
-              alt={`Cover of ${title}`}
-            />
+            <figure>
+              <Image
+                src={image.src}
+                width={image.width}
+                height={image.height}
+                alt={`Image of ${title}`}
+              />
+            </figure>
           ) : null}
           <div className="flex flex-col">
-            <p className="text-2xl text-purple-600 font-semibold group-hover:underline">
-              {title}
-            </p>
+            <p className="text-2xl text-purple-600 font-semibold">{title}</p>
             <p className="font-semibold">{author}</p>
             {description ? (
               <p className="mt-1 italic">‘{description}’</p>
