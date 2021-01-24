6aa7da694e377cca9c2e587ab018177435296975
Angel Paunchev <angel@ebag.bg>
2021-01-24 12:52:54 +0200

    Add more library items

--- components/LibraryItem.js
+++ components/LibraryItem.js
@@ -5,23 +5,27 @@ export default function LibraryItem({
   title,
   author,
   description,
+  quote,
   image,
   url,
 }) {
   return (
-    <a
-      className="focus:ring-2 focus:ring-purple-600 ring-offset-2 outline-none rounded transition"
-      href={url}
-      target="_blank"
-      rel="noopener"
-    >
+    <a className="group" href={url} target="_blank" rel="noopener">
       <div className="flex flex-col items-start">
         <small className="inline-block mb-2 py-1 px-2 rounded bg-gray-800 font-bold text-gray-200 text-xs uppercase tracking-tight">
           {type}
         </small>
-        <div className="flex items-start">
+        <div className="flex w-full">
+          <div className="flex flex-col flex-1">
+            <p className="text-2xl text-purple-600 font-semibold group-hover:text-gray-700">
+              {title}
+            </p>
+            <p className="font-semibold">{author}</p>
+            {description ? <p className="mt-1">{description}</p> : null}
+            {quote ? <p className="mt-1 italic">‘{quote}’</p> : null}
+          </div>
           {image ? (
-            <figure>
+            <figure className="ml-4">
               <Image
                 src={image.src}
                 width={image.width}
@@ -30,13 +34,6 @@ export default function LibraryItem({
               />
             </figure>
           ) : null}
-          <div className="flex flex-col">
-            <p className="text-2xl text-purple-600 font-semibold">{title}</p>
-            <p className="font-semibold">{author}</p>
-            {description ? (
-              <p className="mt-1 italic">‘{description}’</p>
-            ) : null}
-          </div>
         </div>
       </div>
     </a>
--- pages/library.js
+++ pages/library.js
@@ -2,21 +2,39 @@ import Layout from '@/components/Layout';
 import LibraryItem from '@/components/LibraryItem';
 import PageHeader from '@/components/PageHeader';
 import library from '@/data/library.json';
+import useData from 'hooks/useData';
+import { useState } from 'react';
 
 const pageInfo = {
   title: 'Library',
-  description: 'My favourite content in one place.',
+  description:
+    'A public library of sorts, showcasing briliant, insightful and inspiring content and people from around the web.',
 };
 
 export default function Library() {
+  const [filter, setFilter] = useState('');
+  const filteredLibrary = useData(library.data, filter);
+
+  const onFilterChange = event => setFilter(event.target.value);
+
   return (
     <Layout {...pageInfo}>
       <PageHeader title={pageInfo.title} subtitle={pageInfo.description} />
-      <div className="grid grid-cols-library gap-12">
-        {library.data.map(item => (
-          <LibraryItem key={item.url} {...item} />
+      <select className="mb-8 p-2" onChange={onFilterChange}>
+        <option value="">filter by type</option>
+        {library.types.map(type => (
+          <option key={type} value={type}>
+            {type}
+          </option>
         ))}
-      </div>
+      </select>
+      {filteredLibrary.items.length > 0 ? (
+        <div className="grid grid-cols-library gap-12">
+          {filteredLibrary.items.map(item => (
+            <LibraryItem key={item.url} {...item} />
+          ))}
+        </div>
+      ) : null}
     </Layout>
   );
 }
