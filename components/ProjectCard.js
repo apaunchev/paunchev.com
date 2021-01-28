99b0b3223ae949eb8becab98b75c2b7386af9e85
Angel Paunchev <apaunchev@gmail.com>
2021-01-28 18:59:47 +0200

    Add project tags

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,4 +1,4 @@
-export default function ProjectCard({ name, description, url }) {
+export default function ProjectCard({ name, description, url, tags }) {
   return (
     <a
       href={url}
@@ -6,12 +6,19 @@ export default function ProjectCard({ name, description, url }) {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div>
-        <h2 className="text-lg font-bold tracking-tight">{name}</h2>
-        <p className="my-0 text-base text-gray-700 dark:text-gray-400">
-          {description}
-        </p>
-      </div>
+      <h2 className="text-lg font-bold tracking-tight">{name}</h2>
+      <p className="my-0 text-base text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+      {tags && tags.length ? (
+        <ul className="flex">
+          {tags.map(tag => (
+            <li className="mt-3 mr-2 py-1 px-3 bg-gray-100 lowercase text-gray-900 text-sm dark:bg-gray-600 dark:text-gray-300">
+              {tag}
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </a>
   );
 }
