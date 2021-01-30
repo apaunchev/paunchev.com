a096b1eba35b2684aee0f1c529d70e9e07eca224
Angel Paunchev <apaunchev@gmail.com>
2021-01-30 13:55:44 +0200

    Make use of _document.js

--- components/Container.js
+++ components/Container.js
@@ -5,33 +5,12 @@ export default function Container({ children, title, description }) {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
         <title>{title ? `${title} – ` : null}Angel Paunchev</title>
         {description ? <meta name="description" content={description} /> : null}
       </Head>
-      <div className="px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16">
-        <div className="flex flex-col space-y-6 md:space-y-9 lg:space-y-12">
-          <Header />
-          <main>{children}</main>
-        </div>
+      <div className="flex flex-col space-y-6 md:space-y-9 lg:space-y-12">
+        <Header />
+        <main>{children}</main>
       </div>
     </>
   );
---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </Head>
+        <body className="px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
