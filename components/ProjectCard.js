075ceaaf2cb23f677bef9b0ae029fbfc72f42827
Angel Paunchev <apaunchev@gmail.com>
2021-01-30 12:44:46 +0200

    Add missing key

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -13,7 +13,10 @@ export default function ProjectCard({ name, description, url, tags }) {
       {tags && tags.length ? (
         <ul className="flex">
           {tags.map(tag => (
-            <li className="mt-3 mr-2 py-1 px-3 bg-gray-100 lowercase text-gray-900 text-sm dark:bg-gray-600 dark:text-gray-300">
+            <li
+              key={tag}
+              className="mt-3 mr-2 py-1 px-3 bg-gray-100 lowercase text-gray-900 text-sm dark:bg-gray-600 dark:text-gray-300"
+            >
               {tag}
             </li>
           ))}
