08c05ff25f36968150ce886774d9516d793406fc
Angel Paunchev <angel@ebag.bg>
2022-04-23 19:56:52 +0300

    Refactor API functions

--- pages/api/github/repository/[...repository].js
+++ pages/api/github/repository/[...repository].js
@@ -1,14 +1,11 @@
 const GITHUB_API = 'https://api.github.com';
-
-const GITHUB_ENDPOINT = (user, repository) => {
-  return `${GITHUB_API}/repos/${user}/${repository}`;
-};
-
+const GITHUB_REPO_ENDPOINT = (user, repository) =>
+  `${GITHUB_API}/repos/${user}/${repository}`;
 const GITHUB_HEADERS = new Headers({
   Authorization: `Token ${process.env.GITHUB_ACCESS_TOKEN}`,
 });
 
-export const STALE_DURATION = 3600;
+export const STALE_DURATION = 86400;
 export const FRESH_DURATION = STALE_DURATION / 2;
 
 export default async function repository(req, res) {
@@ -16,24 +13,23 @@ export default async function repository(req, res) {
     const {
       repository: [user, repository],
     } = req.query;
-    const response = await fetch(GITHUB_ENDPOINT(user, repository), {
+    const response = await fetch(GITHUB_REPO_ENDPOINT(user, repository), {
       headers: GITHUB_HEADERS,
-    }).then(response => {
-      if (!response.ok) throw new Error();
-
-      return response.json();
     });
+    const json = await response.json();
 
-    res.setHeader(
-      'Cache-Control',
-      `public, s-maxage=${FRESH_DURATION}, max-age=${FRESH_DURATION}, stale-while-revalidate=${STALE_DURATION}`,
-    );
-    res.status(200).json({
-      name: response.name,
-      description: response.description,
-      stars: response.stargazers_count,
-      language: response.language,
-    });
+    res
+      .setHeader(
+        'Cache-Control',
+        `public, s-maxage=${FRESH_DURATION}, max-age=${FRESH_DURATION}, stale-while-revalidate=${STALE_DURATION}`,
+      )
+      .status(200)
+      .json({
+        name: json.name,
+        description: json.description,
+        stars: json.stargazers_count,
+        language: json.language,
+      });
   } catch (err) {
     res.status(500).send(err);
   }
--- pages/api/letterboxd/latest.js
+++ pages/api/letterboxd/latest.js
@@ -6,37 +6,35 @@ const LETTERBOXD_URL = 'https://letterboxd.com';
 const LETTERBOXD_FEED = `${LETTERBOXD_URL}/${LETTERBOXD_USERNAME}/rss/`;
 const LETTERBOXD_FILM_URL = film => `${LETTERBOXD_URL}/film/${film}/`;
 
-export const STALE_DURATION = 3600;
+export const STALE_DURATION = 86400;
 export const FRESH_DURATION = STALE_DURATION / 2;
 
 export default async function latestFilm(req, res) {
   try {
-    const response = await fetch(LETTERBOXD_FEED).then(response => {
-      if (!response.ok) throw new Error();
-
-      return response.text();
-    });
-
+    const response = await fetch(LETTERBOXD_FEED);
+    const xml = await response.text();
     const parser = new XMLParser();
-    const feed = parser.parse(response);
+    const feed = parser.parse(xml);
     const film = feed.rss.channel.item[0];
     const [poster] =
       film.description.match(/(http(s?):)([\s\w./|-])*\.jpg/) ?? [];
     const [, slug] = film.link.match(/film\/([^/]*)\/?/) ?? [];
 
-    res.setHeader(
-      'Cache-Control',
-      `public, s-maxage=${FRESH_DURATION}, max-age=${FRESH_DURATION}, stale-while-revalidate=${STALE_DURATION}`,
-    );
-    res.status(200).json({
-      film,
-      title: decode(film['letterboxd:filmTitle']),
-      year: film['letterboxd:filmYear'],
-      rating: film['letterboxd:memberRating'],
-      date: film['letterboxd:watchedDate'],
-      poster,
-      url: LETTERBOXD_FILM_URL(slug),
-    });
+    res
+      .setHeader(
+        'Cache-Control',
+        `public, s-maxage=${FRESH_DURATION}, max-age=${FRESH_DURATION}, stale-while-revalidate=${STALE_DURATION}`,
+      )
+      .status(200)
+      .json({
+        film,
+        poster,
+        title: decode(film['letterboxd:filmTitle']),
+        year: film['letterboxd:filmYear'],
+        rating: film['letterboxd:memberRating'],
+        date: film['letterboxd:watchedDate'],
+        url: LETTERBOXD_FILM_URL(slug),
+      });
   } catch (err) {
     res.status(500).send(err);
   }
