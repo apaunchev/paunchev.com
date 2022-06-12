6e020dc34dd0e537484b0a9ef96f8c805e32147f
Angel Paunchev <apaunchev@gmail.com>
2022-06-12 12:35:01 +0300

    Preload API requests

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Activity } from 'components/home-sections/activity';
 import { Intro } from 'components/home-sections/intro';
 import { Resume } from 'components/home-sections/resume';
@@ -6,6 +7,20 @@ import { Page } from 'layouts/page';
 export default function HomePage() {
   return (
     <Page>
+      <Head>
+        <link
+          as="fetch"
+          crossOrigin="anonymous"
+          href="/api/goodreads/latest"
+          rel="preload"
+        />
+        <link
+          as="fetch"
+          crossOrigin="anonymous"
+          href="/api/letterboxd/latest"
+          rel="preload"
+        />
+      </Head>
       <Intro />
       <Resume />
       <Activity />
