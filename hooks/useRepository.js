34f96b97dcb2363c2eaf9de4b182706b2f05c767
Angel Paunchev <apaunchev@gmail.com>
2022-07-08 18:53:06 +0300

    Redo projects page

--- hooks/useRepository.js
+++
@@ -1,8 +0,0 @@
-import { fetcher } from 'lib/fetcher';
-import useSWR from 'swr/immutable';
-
-export function useRepository(repository) {
-  const { data } = useSWR(`/api/github/repository/${repository}`, fetcher);
-
-  return data ?? {};
-}
--- pages/projects.js
+++ pages/projects.js
@@ -1,62 +1,47 @@
-import clsx from 'clsx';
-import { Star } from 'react-feather';
-import { useRepository } from 'hooks/useRepository';
-import { Card, CardGrid } from 'components/layout/card-grid';
-import { Skeleton } from 'components/content/skeleton';
 import { Page } from 'layouts/page';
-
-const projects = [
-  'apaunchev/playground',
-  'apaunchev/Teamboard',
-  'apaunchev/GitObserve',
-  'apaunchev/paunchev.com',
-];
+import Image from 'next/image';
+import playground from 'public/projects/playground.png';
 
 const pageInfo = {
   title: 'Projects',
-  description: 'Personal open-source projects I (try to) maintain.',
+  description: 'Personal open-source projects.',
 };
+
+const projects = [
+  {
+    name: 'Playground',
+    description:
+      'A browser playground for HTML, CSS and JavaScript. Designed for quick prototyping and experimenting with React components.',
+    image: playground,
+    websiteUrl: 'https://pg.paunchev.com',
+    githubUrl: 'https://github.com/apaunchev/playground',
+  },
+];
 
 export default function Projects() {
   return (
     <Page title={pageInfo.title} description={pageInfo.description}>
-      <CardGrid>
-        {projects.map(p => (
-          <Project key={p} repository={p} />
-        ))}
-      </CardGrid>
+      {projects.map(project => (
+        <Project key={project.name} {...project} />
+      ))}
     </Page>
   );
 }
 
-function Project({ repository }) {
-  const { name, description, stars, language } = useRepository(repository);
-
+function Project({ name, description, image, websiteUrl, githubUrl }) {
   return (
-    <Card
-      url={`https://github.com/${repository}`}
-      title={name ?? <Skeleton className="!w-40" />}
-      description={description ?? <Skeleton className="!w-80" />}
-      meta={
-        <>
-          <div className="flex items-center gap-1">
-            <span
-              className={clsx(
-                'inline-block w-3 h-3 rounded-full align-middle',
-                {
-                  'bg-zinc-400': language === undefined,
-                  'bg-yellow-300': language === 'JavaScript',
-                  'bg-blue-900': language === 'TypeScript',
-                },
-              )}
-            />
-            {language ?? <Skeleton />}
-          </div>
-          <div className="flex items-center gap-1">
-            <Star size={16} /> {stars ?? <Skeleton />}
-          </div>
-        </>
-      }
-    />
+    <div className="prose">
+      <a href={websiteUrl} className="block text-[0px]">
+        <Image src={image} alt={name} />
+      </a>
+      <h2 className="mt-3 mb-1 text-2xl font-semibold leading-snug">{name}</h2>
+      <p className="mb-0">{description}</p>
+      {websiteUrl || githubUrl ? (
+        <div className="mt-1 flex gap-4">
+          {websiteUrl ? <a href={websiteUrl}>Visit website</a> : null}
+          {githubUrl ? <a href={githubUrl}>See code on GitHub</a> : null}
+        </div>
+      ) : null}
+    </div>
   );
 }
