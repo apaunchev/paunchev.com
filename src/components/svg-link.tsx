c89c0a3de95af0158fbcf1a6c732368875177f22
Angel Paunchev <angel@ebag.bg>
2022-11-05 18:42:03 +0200

    Add labels

--- src/components/svg-link.tsx
+++ src/components/svg-link.tsx
@@ -1,13 +1,16 @@
 export function SVGLink({
   href,
+  label,
   children,
 }: {
   href: string;
+  label: string;
   children: React.ReactNode;
 }) {
   return (
     <a
       href={href}
+      aria-label={label}
       className="flex h-5 w-5 fill-zinc-400 transition-colors hover:fill-zinc-800"
     >
       {children}
