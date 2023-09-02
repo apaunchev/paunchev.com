d46db31546d94f88d042f50430b155e5a33b2d5a
Angel Paunchev <apaunchev@gmail.com>
2023-09-02 12:43:38 +0300

    Remove custom asset naming

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: "assets/[hash][extname]",
-        },
-      },
-    },
-  },
   integrations: [tailwind()],
 });
